1fea5ea262f5f88b5de8550aaec2ecc71490a8df
Omg577 <66808489+Omg577@users.noreply.github.com>
2023-06-02 23:16:58 -0700

    cursor stuff

---
+++ AmoongusPizzaria/Assets/Scripts/ShowCursor.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCursor : MonoBehaviour
+{
+    [SerializeField]
+    private Texture2D ReleasedState;
+    [SerializeField]
+    private Texture2D PressedState;
+    [SerializeField]
+    private Vector2 _hotspot;
+
+    [SerializeField]
+    private CursorMode _cursorMode = CursorMode.Auto;
+
+    void Start()
+    {
+        Cursor.SetCursor(ReleasedState, _hotspot, _cursorMode);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+          Cursor.SetCursor(PressedState, _hotspot, _cursorMode);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+          Cursor.SetCursor(ReleasedState, _hotspot, _cursorMode);
+        }
+    }
+
+/*    private void OnMouseUp()
+    {
+        Cursor.SetCursor(ReleasedState, _hotspot, _cursorMode);
+    }
+
+    private void OnMouseDown()
+    {
+        Cursor.SetCursor(PressedState, _hotspot, _cursorMode);
+    }*/
+}
