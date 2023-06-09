c12a8e88348f51d3ec5998a94505c5c5642be723
Omg577 <66808489+Omg577@users.noreply.github.com>
2023-06-09 07:45:10 -0700

    Update ToppingDistanceCalculator.cs

--- AmoongusPizzaria/Assets/Scripts/ToppingDistanceCalculator.cs
+++ AmoongusPizzaria/Assets/Scripts/ToppingDistanceCalculator.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class ToppingDistanceCalculator : MonoBehaviour
 {
     public GameObject parentObject;
+    public int pairCount = 0;
+    public float totalDistance = 0f;
+    public float averageDistance;
     private void Start()
     {
         String parentObjectName = this.parentObject.name;
@@ -23,10 +26,17 @@ public class ToppingDistanceCalculator : MonoBehaviour
             for (int i = 0; i < children.Length; i++)
             {
                 for (int j = i + 1; j < children.Length; j++)
-                {
+                {   
                     float distance = Vector3.Distance(children[i].position, children[j].position);
 
+                    totalDistance += distance;
+                    pairCount++;
+
+                    averageDistance = totalDistance / pairCount;
+
                     Debug.Log("Distance between " + children[i].name + " and " + children[j].name + ": " + distance);
+                    Debug.Log("Average distance between child objects: " + averageDistance);
+
                 }
             }
         }
