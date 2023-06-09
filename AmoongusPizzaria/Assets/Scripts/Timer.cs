b1bf4c139c98bb59b4b680b3a8373c91d4b7a16d
gfyfaye <gogograce999@gmail.com>
2023-06-09 04:59:23 -0700

    timer, ending scene, moved and fixed sauces, pizza box

---
+++ AmoongusPizzaria/Assets/Scripts/Timer.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [Header("Component")]
+    public TextMeshProUGUI timerText;
+
+    [Header("Timer Settings")]
+    public float currentTime;
+    public bool countDown;
+
+    [Header("Limit Settings")]
+    public bool hasLimit;
+    public float timerLimit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = countDown ? currentTime -= Time.deltaTime : currentTime += Time.deltaTime;
+
+        if(hasLimit && ((countDown && currentTime <= timerLimit) || (!countDown && currentTime >= timerLimit)))
+        {
+            currentTime = timerLimit;
+            enabled = false;
+        }
+        if (currentTime <= 10)
+        {
+            timerText.color = Color.red;
+        }
+        if (currentTime == 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        SetTimerText();
+    }
+
+    private void SetTimerText()
+    {
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+        timerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+    }
+}
--- AmoongusPizzaria/Assets/Scripts/cheeseandsauce.cs
+++ AmoongusPizzaria/Assets/Scripts/cheeseandsauce.cs
@@ -5,20 +5,43 @@ using UnityEngine;
 public class cheeseandsauce : MonoBehaviour
 {
     public GameObject interactObject;
+    public Transform parentObject;
     public SpriteRenderer spriteRenderer;
+    public Sprite containerObject;
     public Sprite placedObject;
+    public float scalingSize;
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = containerObject;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (interactObject != null && interactObject.GetComponent<Collider2D>().bounds.Intersects(spriteRenderer.bounds))
+        if (interactObject != null && interactObject.GetComponent<Collider2D>().bounds.Intersects(spriteRenderer.bounds)){
             this.gameObject.GetComponent<SpriteRenderer>().sprite = placedObject;
-            this.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 5;
+            //this.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 5;
+
+            SpreadOut();
+
+                    // Make the parent object the new parent of the object
+                    this.gameObject.transform.SetParent(parentObject);
+
+                    // Reset the position of the dragging object relative to the parent object
+                    //draggingObject.transform.localPosition = initialPosition; // it would set the local position to the intial position - middle of the gameobject then
+
+                    // Enable the parent object's movement script
+                    parentObject.GetComponent<MoveParentObject>().enabled = true;
+        }
+    }
+
+    private void SpreadOut() {
+
+        if (this.gameObject.GetComponent<SpriteRenderer>().sprite = placedObject){
+            this.gameObject.transform.localScale = new Vector3(scalingSize, scalingSize, scalingSize);
+        }
     }
 }
